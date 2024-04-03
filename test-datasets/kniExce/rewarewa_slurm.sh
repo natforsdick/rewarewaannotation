4471eb285089c05e33ce328ce3eca4c1595e5f8f
Natalie Forsdick <20059249+natforsdick@users.noreply.github.com>
2024-04-04 10:59:14 +1300

    correcting for currently functioning version

--- test-datasets/kniExce/rewarewa_slurm.sh
+++ test-datasets/kniExce/rewarewa_slurm.sh
@@ -1,24 +1,30 @@
 #!/bin/bash -l
-#SBATCH --job-name=rewarewaannotation
+#SBATCH --job-name=annotation
+#SBATCH --account=ACCOUNTID # set to NeSI account ID
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --time 120:00:00
-
+#SBATCH --mem=3G
+#SBATCH --time 02:00:00 # as yet unclear how long a full run with 1 Gb genome will take - perhaps up to 5-00:00:00
+#SBATCH --mail-user=EMAIL # insert user email
+#SBATCH --mail-type=ALL
 #SBATCH --output=slurm-%x-%A-%a.log
 
-# run from current directory
-cd $SLURM_SUBMIT_DIR
+# ensure that paths to cache and tmpdir are functional
+setfacl -b "${NXF_SINGULARITY_CACHEDIR}" /path/to/rewarewaannotation/main.nf
+setfacl -b "${SINGULARITY_TMPDIR}" /nesi/nobackup/landcare03691/data/output/07-annotation/rewarewaannotation/main.nf
 
-# command to use
-module load singularity
-module load anaconda
+# run from output directory
+cd /path/to/outdir/
 
-conda activate nextflow
+# NeSI environment
+module load Java/11.0.4 Singularity/3.11.3 
 
-nextflow run kherronism/rewarewaannotation \
-   -params-file rewarewa_params.yml \
-   -profile sonic \
-   -resume
+nextflow -version
 
+# to run the full pipeline
+nextflow run /path/to/rewarewaannotation/ -profile nesi,singularity -params-file /path/to/params.yml
+# to resume a run, append -resume to the above
+
+module purge
